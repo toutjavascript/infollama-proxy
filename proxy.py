08d8cba8d013d784cba0fa3040bf018372e93fe2
TJS <30899600+toutjavascript@users.noreply.github.com>
2025-01-10 17:33:13 +0100

    stream is working :)

--- proxy.py
+++ proxy.py
@@ -4,8 +4,17 @@ Author: toutjavascript.com
 Created: 2025-01
 """
 import requests
+import os
+import multiprocessing
+from flask_cors import CORS
 import src.pytherminal as pytherminal
 import src.device    as device
+import src.utils     as utils
+from flask import Flask, render_template, request, send_from_directory, Response, stream_with_context
+
+# Constants to identify version and execution modalities
+OLLAMA_PROXY_RELEASE="0.0.1"
+OLLAMA_MIN_RELEASE="0.5.0"
 
 class InfollamaProxy:
     def __init__(self, port=11430, base_url="http://localhost:11434/"):
@@ -39,10 +48,19 @@ class InfollamaProxy:
         except Exception as e:
             print(e);
         return response.json()      
+    
     def post(self, endpoint, data=None, **kwargs):
         url = self.create_url(endpoint)        
         response = requests.post(url, json=data, params=kwargs)
-        return response.json()
+        # Check if the response is streamed
+        #print("data", data)
+        if data["stream"]==True:
+            print("Streaming data")
+            for chunk in response.iter_content(chunk_size=1024): 
+                stream_Data=chunk.decode('utf-8')   # Get each chunk of data
+                return Response(stream_Data, mimetype='text/event-stream')
+        else:
+            return response.json()
     def put(self, endpoint, data=None, **kwargs):
         url = self.create_url(endpoint)
         response = requests.put(url, json=data, params=kwargs)
@@ -66,6 +84,12 @@ class InfollamaProxy:
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    tjs_host="localhost"
+    tjs_port=11430
+    cors_policy="*"
+
     proxy = InfollamaProxy()
     response = proxy.check_connection()
     print(response)
@@ -74,4 +98,48 @@ if __name__ == "__main__":
     print(response)
 
     print(device.getDeviceInfo())  
+    
+    appPath=utils.getAppPath()
+    app = Flask(__name__)
+    if cors_policy == "*":
+        CORS(app)  # Enable CORS for all routes
 
+    @app.route('/')
+    def home():
+        return render_template('index.html', release=OLLAMA_PROXY_RELEASE)
+
+    @app.route('/favicon.png', methods=['GET'])
+    def serveFavicon():
+        return send_from_directory(os.path.join(appPath, 'static/picto'), 'favicon.png', mimetype='image/vnd.microsoft.icon')
+
+    @app.route('/robots.txt', methods=['GET'])
+    def serveRobots():
+        return "deny: all"
+
+
+    def stream_request(method, url, **kwargs):
+        with requests.request(method, url, stream=True, **kwargs) as r:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    yield chunk
+
+    # Serve all the other endpoints with flask
+    @app.route('/<path:path>', methods=['GET', 'POST', 'OPTIONS'])
+    def catch_all(path):
+        if request.method == 'GET':
+            return proxy.get(path, **request.args)
+        elif request.method == 'POST':
+            # print("Method POST received", "path: ", path, "json: ", request.json)
+            #return proxy.post(path, request.json, **request.args)
+            url=proxy.create_url(path)
+            print(url)
+            return Response(stream_with_context(stream_request('POST', url, json=request.json, params=request.args)), content_type=request.headers.get('Content-Type'))
+    
+        elif request.method == 'OPTIONS':
+            # Handle CORS preflight requests
+            return '', 204 
+        else:
+            return "Method not allowed", 405
+
+
+    app.run(host=tjs_host, port=tjs_port)
